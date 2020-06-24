3b7aac6cb9ae7e1791092d608f81e8cee30991e0
tanothierry <tano.tano@uvci.edu.ci>
2020-06-24 13:23:13 +0000

    new

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,35 @@
-userController = require('../controller/user_controller');
+userController = require("../controller/user_controller");
 
-const { profil } = require('../controller/upload');
+const { profil } = require("../controller/upload");
 
-userMediaControler = require('../controller/user_media_controller');
-userMessages = require('../controller/user_message');
+userMediaControler = require("../controller/user_media_controller");
+userMessages = require("../controller/user_message");
 module.exports = (app) => {
-  app.post('/user/create/', userController.create);
-  app.post('/user/connect/', userController.connect);
-  app.get('/user/all/', userController.getAllUsers);
-  app.post('/user/addNew/', userController.addNewContact);
-  app.post('/user/updateProfil', profil, async (req, res) => {
+  app.get("/", (req, res) => {
+    res.render("index");
+  });
+  app.post("/user/create/", userController.create);
+  app.post("/user/connect/", userController.connect);
+  app.get("/user/all/", userController.getAllUsers);
+  app.post("/user/addNew/", userController.addNewContact);
+  app.post("/user/updateProfil", profil, async (req, res) => {
     let data = { ...req.body, file: req.file.filename };
     let output = await userMediaControler.updateUserProfil(data);
     res.json(output);
   });
-  app.post('/contact/all/', userController.getAllContacts);
-  app.post('/messages/all/', async (req, res) => {
+  app.post("/contact/all/", userController.getAllContacts);
+  app.post("/messages/all/", async (req, res) => {
     let data = req.body.user;
     let output = await userMessages.getAllMessages(data);
     res.json(output);
   });
-  app.post('/messages/add', async (req, res) => {
+  app.post("/messages/add", async (req, res) => {
     let data = req.body;
     let output = await userMessages.addMessage(data);
     res.json(output);
   });
 
-  app.post('/messages/delete/', async (req, res) => {
+  app.post("/messages/delete/", async (req, res) => {
     let output = await userMessages.deleteMessage(req.body);
 
     res.json(output);
