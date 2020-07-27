61688a22c263821dc30d35041dd31bdd375b0f7b
assan1997 <nicolasassan7@gmail.com>
2020-07-27 16:35:38 +0000

    new

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ router.route("/addUser").post(async (req, res) => {
 });
 
 router.route("/addGroup").post(async (req, res) => {
-  req.body.users = req.body.users.split(' ');
+  req.body.users = req.body.users.split(' ').splice(req.body.users.split(' ').length-1,1);
   req.body.id_ent = parseInt(req.body.id_ent);
   console.log("body addGroup", req.body);
   const s = await globalQueries.addGroup(req.body);
